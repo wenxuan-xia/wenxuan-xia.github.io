2b353959158fa92786acea7c70fadeec7c65c024
wenxuan-xia <wenxuan.xia@hotmail.com>
2015-02-14 23:59:30 +0800

    hw_2 finish

--- js/hw_2.js
+++ js/hw_2.js
@@ -1,22 +1,56 @@
 function load_stu_score() {
-	url = "http://yijisoo.github.io/ie59000/2015spring/hw2/students.json"//local
+	url = "./data/students.json"//local
 	$.get(
 			url,
 			{},
 			function(o) {
 				str = ""
+				data = data_process(o)
 				tbody = document.getElementById('student_json_loader')
 				for (var i = 0; i<o.length; i++) {
-					tr = tbody.insertRow();
-					tr.insertCell().innerHTML = o[i].Name;
-					tr.insertCell().innerHTML = o[i].GPA;
-					tr.insertCell().innerHTML = o[i].GRE_V;
-					tr.insertCell().innerHTML = o[i].GRE_Q;
-					tr.insertCell().innerHTML = o[i].Essay;
-					tr.insertCell().innerHTML = o[i].Recom;
+					tr = tbody.insertRow()
+					tr.insertCell().innerHTML = data[i].Name
+					tr.insertCell().innerHTML = data[i].GPA
+					tr.insertCell().innerHTML = data[i].GRE_V
+					tr.insertCell().innerHTML = data[i].GRE_Q
+					tr.insertCell().innerHTML = data[i].Essay
+					tr.insertCell().innerHTML = data[i].Recom
+					tr.insertCell().innerHTML = data[i].total.toFixed(4)
+					tr.insertCell().innerHTML = data[i].ranking
 				};
 
 			},
 			"json"
 		);
+}
+
+function data_process(o) {
+	for (var i=0; i<o.length; i++) {
+		o[i].total = o[i].GPA/4 + (o[i].GRE_V-130)/40 + (o[i].GRE_Q-130)/40 + o[i].Essay/6 + o[i].Recom/10
+		o[i].mark = i
+	}
+	for (var i=0; i<o.length; i++) {
+		for (var j=i+1; j<o.length; j++) {
+			if (o[i].total < o[j].total) {
+				t = o[i]
+				o[i] = o[j]
+				o[j] = t
+			}
+		}
+	}
+	
+	for (var i=0; i<o.length; i++) {
+		o[i].ranking = i+1
+	}
+
+	for (var i=0; i<o.length; i++) {
+		for (var j=i+1; j<o.length; j++) {
+			if (o[i].mark > o[j].mark) {
+				t = o[i]
+				o[i] = o[j]
+				o[j] = t
+			}
+		}
+	}
+	return o
 }
