51d346743409e954e28027d3694c2aa6a966cd81
wenxuan-xia <wenxuan.xia@hotmail.com>
2015-02-16 01:05:42 +0800

    change getJSON to d3.json

--- js/hw_2.js
+++ js/hw_2.js
@@ -1,27 +1,20 @@
 function load_stu_score() {
-	url = "./data/students.json"//local
-	$.get(
-			url,
-			{},
-			function(o) {
-				str = ""
-				data = data_process(o)
-				tbody = document.getElementById('student_json_loader')
-				for (var i = 0; i<o.length; i++) {
-					tr = tbody.insertRow()
-					tr.insertCell().innerHTML = data[i].Name
-					tr.insertCell().innerHTML = data[i].GPA
-					tr.insertCell().innerHTML = data[i].GRE_V
-					tr.insertCell().innerHTML = data[i].GRE_Q
-					tr.insertCell().innerHTML = data[i].Essay
-					tr.insertCell().innerHTML = data[i].Recom
-					tr.insertCell().innerHTML = data[i].total.toFixed(4)
-					tr.insertCell().innerHTML = data[i].ranking
-				};
-
-			},
-			"json"
-		);
+	url = "https://hivelab.org/static/students.json"
+	d3.json(url, function(o){
+       		data = data_process(o)
+			tbody = document.getElementById('student_json_loader')
+			for (var i = 0; i<o.length; i++) {
+				tr = tbody.insertRow()
+				tr.insertCell().innerHTML = data[i].Name
+				tr.insertCell().innerHTML = data[i].GPA
+				tr.insertCell().innerHTML = data[i].GRE_V
+				tr.insertCell().innerHTML = data[i].GRE_Q
+				tr.insertCell().innerHTML = data[i].Essay
+				tr.insertCell().innerHTML = data[i].Recom
+				tr.insertCell().innerHTML = data[i].total.toFixed(4)
+				tr.insertCell().innerHTML = data[i].ranking
+			};
+        });
 }
 
 function data_process(o) {
@@ -29,6 +22,7 @@ function data_process(o) {
 		o[i].total = o[i].GPA/4 + (o[i].GRE_V-130)/40 + (o[i].GRE_Q-130)/40 + o[i].Essay/6 + o[i].Recom/10
 		o[i].mark = i
 	}
+	
 	for (var i=0; i<o.length; i++) {
 		for (var j=i+1; j<o.length; j++) {
 			if (o[i].total < o[j].total) {
